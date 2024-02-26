482ebf13b6ff5b9ae99b29452e039ac583f19f3e
GabrielLima2803 <gabriellima2803@gmail.com>
2024-02-26 11:57:00 -0300

    Criação dos serializeres livro e autor

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,3 +1,5 @@
 from .user import UserSerializer
 from .categoria import CategoriaSeralizer
 from .editora import EditoraSerializer
+from .autor import AutorSeralizer
+from .livro import LivroSerializer, LivroListSerializer, LivroDetailSerializer
---
+++ core/serializers/autor.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Autor
+
+class AutorSeralizer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
