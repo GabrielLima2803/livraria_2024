24649de8c4cfd237bb31e24b55959c47640d7d23
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-04 11:51:22 -0300

    Testando permission_classes em LivrosView

--- core/views/livro.py
+++ core/views/livro.py
@@ -3,10 +3,13 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Livro
 from core.serializers import LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-
+    permission_classes = [IsAuthenticated]
+    
     def get_serializer_class(self):
         if self.action == "list":
             return LivroListSerializer
