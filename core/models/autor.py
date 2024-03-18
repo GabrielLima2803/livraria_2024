ed6a35d43841685ed0d8be6d744cb711e86a8f90
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-18 11:41:04 -0300

    Fix Campo Email no Autor

--- core/models/autor.py
+++ core/models/autor.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
 
     class Meta:
         verbose_name_plural = "Autores"
